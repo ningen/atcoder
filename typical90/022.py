ee0266ae25d4567c2982383d112dfa783575f065
ningen <ningen361@gmail.com>
2025-05-11 21:11:00 +0900

    moevd

--- typical90/022.py
+++
@@ -1,8 +0,0 @@
-from math import gcd
-
-
-a, b, c = map(int, input().split())
-
-num = gcd(a, b, c)
-
-print(a // num - 1 + b // num - 1 + c // num - 1)
--- typical90/033.py
+++
@@ -1,9 +0,0 @@
-# 解説AC
-
-from math import ceil
-H, W = map(int, input().split())
-
-if H == 1 or W == 1:
-    print(H * W)
-else:
-    print(ceil(H / 2) * ceil(W / 2))
